8995777a22f40a402b3aec164aad9c854d012cb1
Caleb Curry <caleb@Calebs-MacBook-Air.local>
2024-01-18 11:12:46 -0500

    basic outline to start a  blockchain

---
+++ src/chain_utils.rs
@@ -0,0 +1,74 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct Blockchain {
+    pub blocks: Vec<Block>,
+    pub accounts: HashMap<String, u128>,
+    pub pending_transactions: Vec<Transaction>,
+}
+#[derive(Debug)]
+pub struct Block {
+    pub transactions: Vec<Transaction>,
+}
+#[derive(Debug)]
+pub struct Transaction {
+    pub from: String,
+    pub to: String,
+    pub amount: u128,
+}
+
+impl Block {
+    pub fn new() -> Self {
+        Block {
+            transactions: Vec::new(),
+        }
+    }
+}
+
+impl Blockchain {
+    pub fn new() -> Self {
+        Blockchain {
+            blocks: Vec::new(),
+            accounts: HashMap::new(),
+            pending_transactions: Vec::new(),
+        }
+    }
+
+    pub fn create_block(&mut self) {
+        let mut block = Block::new();
+        for txn in self.pending_transactions.drain(..) {
+            println!("txn: {txn:?}");
+            //update acct balance
+
+            //genesis transactions don't subtract
+            if self.blocks.len() != 0 {
+                let current_from = self.accounts.entry(txn.from.clone()).or_insert(0);
+                *current_from -= txn.amount;
+            }
+
+            let current = self.accounts.entry(txn.to.clone()).or_insert(0);
+            *current += txn.amount;
+
+            block.transactions.push(txn);
+        }
+        self.blocks.push(block);
+    }
+}
+
+impl Transaction {
+    pub fn new(from: String, to: String, amount: u128) -> Transaction {
+        Transaction { amount, from, to }
+    }
+    pub fn spend(self, blockchain: &mut Blockchain) {
+        //check if valid spend
+
+        if blockchain.blocks.len() != 0 {
+            if let Some(from) = blockchain.accounts.get(&self.from) {
+                if from < &self.amount {
+                    return;
+                }
+            }
+        }
+        blockchain.pending_transactions.push(self);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use chain_utils::Transaction;
+
+mod chain_utils;
 fn main() {
-    println!("Hello, world!");
+    let mut chain = chain_utils::Blockchain::new();
+    let txn = Transaction::new("Caleb".to_string(), "Claire".to_string(), 100);
+    txn.spend(&mut chain);
+
+    println!("{:?}", chain.pending_transactions);
+    println!("{:?}", chain.blocks);
+
+    chain.create_block();
+
+    println!("{:?}", chain.pending_transactions);
+    println!("{:?}", chain.blocks);
+
+    // //should fail
+    // let txn = Transaction::new("Caleb".to_string(), "Claire".to_string(), 100);
+    // txn.spend(&mut chain);
+    // chain.create_block();
+
+    let txn = Transaction::new("Claire".to_string(), "Caleb".to_string(), 50);
+    txn.spend(&mut chain);
+    chain.create_block();
+    println!("{:?}", chain.pending_transactions);
+    println!("{:?}", chain.blocks);
+    println!("{:?}", chain.accounts);
 }
